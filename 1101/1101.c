d20a7c753c93cbeaa7cff3d1228d3089e739e067
José Marciel Bergamo Junior <jmbergamoj@gmail.com>
2017-09-01 23:37:32 -0300

    Update 1101.c

--- 1101/1101.c
+++ 1101/1101.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
 int main(){
+	//variable declaration
 	int x,a,b,s,M,m;
 	do{
+		//input
 		scanf("%d %d",&a,&b);
+		//biggest value to M
 		M=(a>=b)?(a):(b);
+		//lowest value to m
 		m=(a>b)?(b):(a);
+		//stop processing condition
 		if(a<=0||b<=0)
 		break;
+		//output start
 		else{
 			s=0;
 			for(x=m;x<=M;x++){
@@ -16,6 +22,7 @@ int main(){
 			}
 			printf("Sum=%d\n",s);
 		}
+		//end of output
 	}while(a>0||b>0);
 	return 0;
 }
