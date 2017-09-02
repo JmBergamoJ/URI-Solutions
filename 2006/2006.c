68035c7b0249214a3bb6a7f60b11ae7281e56750
José Marciel Bergamo Junior <jmbergamoj@gmail.com>
2017-09-01 23:58:34 -0300

    Update 2006.c

--- 2006/2006.c
+++ 2006/2006.c
@@ -3,25 +3,27 @@
 int main(){
 	int t,a,b,c,d,e,s,x;
 	do{
-	scanf("%d",&t);
-	}while(t<1||t>4);
+		//input tea number
+		scanf("%d",&t);
+	}while(t<1||t>4);//tea must be between 1 and 4
 	do{
-	scanf("%d %d %d %d %d",&a,&b,&c,&d,&e);
-	}while(a<1||a>4||b<1||b>4||c<1||c>4||d<1||d>4||e<1||e>4);
+		//input for contestants choices
+		scanf("%d %d %d %d %d",&a,&b,&c,&d,&e);
+	}while(a<1||a>4||b<1||b>4||c<1||c>4||d<1||d>4||e<1||e>4);//contestant choices must be between 1 and 4
 	s=0;
+	//add 1 to 's' for every time t and contestants choices are matched
+	//can also use a for loop to add 1 to 's'
 	if(t==a)
-	s++;
+		s++;
 	if(t==b)
-	s++;
+		s++;
 	if(t==c)
-	s++;
+		s++;
 	if(t==d)
-	s++;
+		s++;
 	if(t==e)
-	s++;
-	else{
-		
-	}
+		s++;
+	//output
 	printf("%d\n",s);
 	return 0;
 }
