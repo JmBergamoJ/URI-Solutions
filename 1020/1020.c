931aca44c41d32e4a8cb55aa5ef8e4e3f8494c0a
José Marciel Bergamo Junior <jmbergamoj@gmail.com>
2017-09-01 23:49:36 -0300

    Update 1020.c

--- 1020/1020.c
+++ 1020/1020.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
 int main(){
+	//variable declaration
 	int x,d,m,a;
+	//input
 	scanf("%d",&x);
+	//# of years, considering year=365 days
 	a=x/365;
+	//# of months, considering month=30 days
 	m=x%365/30;
+	//# of days
 	d=x%365%30;
+	//start of output
 	printf("%d ano(s)\n",a);
 	printf("%d mes(es)\n",m);
 	printf("%d dia(s)\n",d);
+	//end of output
 	return 0;
 	}	
