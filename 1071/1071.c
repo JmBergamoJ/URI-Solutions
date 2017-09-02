d36ff1f3af141b6b5c4a2c1b7f88f9707cf91593
José Marciel Bergamo Junior <jmbergamoj@gmail.com>
2017-09-02 00:50:39 -0300

    Update 1071.c

--- 1071/1071.c
+++ 1071/1071.c
@@ -3,12 +3,17 @@
 int main(){
 	int x,y,s,M,m,j;
 	s=0;
+	//input
 	scanf("%d %d",&x,&y);
+	//lowest value goes to m
 	m=x>y?(y):(x);
+	//highest value goes to M
 	M=(x>y)?(x):(y);
+	//for every off number, add into 's'
 	for(j=m+1;j<M;j++){
 		if(j%2!=0)
 		s=s+j;
 	}
+	//output
 	printf("%d\n",s);
 }
