7e78bfb1e66ab75ff5db756fcecf9b1e5a65db7a
José Marciel Bergamo Junior <jmbergamoj@gmail.com>
2017-09-01 23:43:50 -0300

    Update 1079.c

--- 1079/1079.c
+++ 1079/1079.c
@@ -3,10 +3,13 @@
 int main(){
 	int x,n;
 	float a,b,c;
+	//input - number of tests
 	scanf("%d",&n);
+	//input for individual tests
 	for(x=0;x<n;x++){
 		scanf("%f %f %f",&a, &b, &c);
-		printf("%.1lf\n",(a*2+b*3+c*5)/10);
+		//output, one digit after the decimal point
+		printf("%.1f\n",(a*2+b*3+c*5)/10);
 	}
 	return 0;
 }
