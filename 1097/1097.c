ca8c42e4e22850ca71636ce8dcf054e34ef29405
José Marciel Bergamo Junior <jmbergamoj@gmail.com>
2017-09-02 00:54:21 -0300

    Update 1097.c

--- 1097/1097.c
+++ 1097/1097.c
@@ -2,11 +2,14 @@
 
 int main(){
 	int a,b,l=1,c=7;
+	//repeats until a>=5
 	for(a=0;a<5;a++){
 		for(b=0;b<3;b++){
+			//output while couting down 'c'
 			printf("I=%d J=%d\n",l,c);
 			c--;
 		}
+		//add 2 to 'l' and 5 to 'c'
 		l=l+2;
 		c=c+5;
 	}
