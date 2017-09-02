e969d9dfbb395d86f76b99e876d5f50d0b090531
José Marciel Bergamo Junior <jmbergamoj@gmail.com>
2017-09-02 00:16:40 -0300

    Update 1099.c

--- 1099/1099.c
+++ 1099/1099.c
@@ -2,16 +2,22 @@
 
 int main (){
 	int t,x,y,i,j,s,M,m;
+	//input - number of tests
 	scanf("%d",&t);
+	//input - individual tests
 	for(i=0;i<t;i++){
 		s=0;
 		scanf("%d %d",&x,&y);
+		//lowest into m
 		m=x>y?(y):(x);
+		//highest into M
 		M=(x>y)?(x):(y);
 		for(j=m+1;j<M;j++){
+			//check if number is odd, and adding 'j' into 's'
 			if(j%2!=0)
-			s=s+j;
+				s=s+j;
 		}
+		//output
 		printf("%d\n",s);
 		s=0;
 	}
